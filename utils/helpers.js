afbddba509df16949bb538720e4fe65f7fb60187
abzflux <pfzlx9@gmail.com>
2025-10-14 21:49:37 +0330

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -34,9 +34,35 @@ const generateKeyboard = (isPersian) => {
   }
 };
 
+// Generate a random color for user avatars
+const generateColor = (str) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const hue = hash % 360;
+  return `hsl(${hue}, 70%, 60%)`;
+};
+
+// Validate game code format
+const isValidGameCode = (code) => {
+  return code && code.length === 6 && /^[A-Z0-9]+$/.test(code);
+};
+
+// Calculate game progress percentage
+const calculateProgress = (word, correctLetters) => {
+  if (!word) return 0;
+  const uniqueLetters = [...new Set(word.split('').filter(char => char !== ' '))];
+  const correctCount = correctLetters.filter(letter => uniqueLetters.includes(letter)).length;
+  return Math.round((correctCount / uniqueLetters.length) * 100);
+};
+
 module.exports = {
   formatTime,
   sanitizeInput,
   isPersian,
-  generateKeyboard
+  generateKeyboard,
+  generateColor,
+  isValidGameCode,
+  calculateProgress
 };
